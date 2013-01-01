798061c9738769fbe9bb72e3dd313a974136e91a
Valdemar Pereira <valdemar.developer@gmail.com>
2013-01-01 16:45:21 +0000

    add gitignore entries and SBT IDEA plugin

--- project/StorytailbackendBuild.scala
+++ project/StorytailbackendBuild.scala
@@ -10,7 +10,23 @@ object StorytailbackendBuild extends Build {
       name := "story-tail-backend",
       organization := "com.valdemar",
       version := "0.1-SNAPSHOT",
-      scalaVersion := "2.9.2"
+      scalaVersion := "2.9.2",
+	resolvers ++= Seq(
+        "akka Releases" at "http://repo.akka.io/releases/",
+        "spray repo" at "http://repo.spray.io",
+        "repo.novus rels" at "http://repo.novus.com/releases/",
+        "Scala tools" at "https://oss.sonatype.org/content/groups/scala-tools/"
+       ),
+      libraryDependencies  ++= Seq(
+        "com.typesafe.akka" % "akka-actor" % "2.0.4",
+        "io.spray" % "spray-can" % "1.0-M7",
+        "io.spray" % "spray-routing" % "1.0-M7",
+        "io.spray" % "spray-http" % "1.0-M7",
+        "org.mongodb" %% "casbah" % "2.3.0",
+        "com.novus" %% "salat-core" % "1.9.1"
+      ),
+        scalacOptions += "-Ydependent-method-types"
+
       // add other settings here
     )
   )
